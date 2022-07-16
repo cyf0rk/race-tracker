ca6dc40e113a7ee7cfb10a73d22600e4de9d7842
marijan111 <marijanbatarilo.dev@gmail.com>
2022-07-16 23:48:21 +0200

    updated index.php

--- index.php
+++ index.php
@@ -5,9 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/index.css">
+    <link rel="stylesheet" href="public/css/form.css">
     <title>Race tracker</title>
 </head>
 <body>
-    <?php require_once __DIR__.'/import_results.php';?>
+    <div class="race-tracker-wrapper">
+        <h1>Track race results</h1>
+        <?php require_once __DIR__.'/templates/import_results.php';?>
+    </div>
 </body>
 </html>
